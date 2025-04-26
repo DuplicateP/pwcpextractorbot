68e33e7f2d3bcd93db24e80fcfa9a3dc5f143187
DuplicateP <modassirjawaid44@gmail.com>
2025-04-26 09:48:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "27058143"
-api_hash = "c569ea4f5ade3fc70d2bb1fd162d9dc9"
-bot_token = "7975806032:AAEyCXWUeYYvSV-LZos1IQZBjVWp-2lCRpc"
-auth_users = [8144269730]
+api_id = "26792945"
+api_hash = "6be4bc36c6a70c269f80a7388e59e150"
+bot_token = "7826299302:AAGtcVKFFPrS0iBa4muuzmB7iGAlY6rqOnc"
+auth_users = [7357284659]
